bbd3feffaec108da54d4c0e353b7c7be3a5a01e0
Kastor Kitov <kitov.kastor@gmail.com>
2024-04-16 17:46:24 +0300

    exersise 2

---
+++ src/FinalExamPrep1/FancyBarcodes_02.java
@@ -0,0 +1,40 @@
+package FinalExamPrep1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FancyBarcodes_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String regexValidBarcode = "@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+";
+        Pattern pattern = Pattern.compile(regexValidBarcode);
+
+        Matcher matcher = null;
+
+        for (int i = 0; i < n; i++) {
+            String barcode = scanner.nextLine();
+
+            matcher = pattern.matcher(barcode);
+
+            if (matcher.find()) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < barcode.length(); j++) {
+                    char symbol = barcode.charAt(j);
+                    if (Character.isDigit(symbol)){
+                        sb.append(symbol);
+                    }
+                }
+                if (sb.length() == 0) {
+                    System.out.println("Product group: 00");
+                } else {
+                    System.out.printf("Product group: %s%n", sb.toString());
+                }
+            } else {
+                System.out.println("Invalid barcode");
+            }
+        }
+    }
+}
